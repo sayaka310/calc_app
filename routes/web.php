715e0f4490b58a7d30074138251f49dd83b7edd3
sayaka310 <syk3887@yahoo.co.jp>
2021-09-16 01:53:09 +0900

    Herokuデプロイのための設定

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'App\Http\Controllers\ItemController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/calcs/{num1}/{operator}/{num2}', [
     App\Http\Controllers\CalcController::class,'result'
