5966c1f22e492b7d3269fee4dc84161881313a29
sayaka310 <syk3887@yahoo.co.jp>
2021-09-15 18:54:22 +0900

    計算アプリ課題完了

---
+++ resources/views/message/calc.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+        <title>calcApp</title>
+</head>
+<body>
+    <h1>計算アプリ</h1>
+    <h2>{{ $result }}</h2>
+</body>
+</html>
